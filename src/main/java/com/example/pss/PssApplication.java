03fd4fb206813683c199b336efcd4dea151540da
munjeongmin <3330kys@dsm.hs.kr>
2022-09-01 17:07:43 +0900

    firstcommit

---
+++ src/main/java/com/example/pss/PssApplication.java
@@ -0,0 +1,13 @@
+package com.example.pss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pss/PssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
