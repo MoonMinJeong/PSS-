1ac64ad306ae33b5c6ba9f566c98205acd276b58
munjeongmin <3330kys@dsm.hs.kr>
2022-09-02 20:09:40 +0900

    ADD :: User domain entity add

---
+++ src/main/java/com/example/pss/domain/user/domain/User.java
@@ -0,0 +1,28 @@
+package com.example.pss.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String imageUrl;
+}
