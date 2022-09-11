cf564b4c8b970962faf9635b9c7481246c28e67b
munjeongmin <3330kys@dsm.hs.kr>
2022-09-12 00:57:23 +0900

    ADD :: Redis config

---
+++ src/main/java/com/example/pss/global/redis/RedisConfig.java
@@ -0,0 +1,18 @@
+package com.example.pss.global.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+}
