ef400b7b67f9058c4a4d764464fb07163d517814
munjeongmin <3330kys@dsm.hs.kr>
2022-09-21 11:33:58 +0900

    🌱 :: userFacade add

---
+++ src/main/java/com/example/pss/domain/user/facade/UserFacade.java
@@ -0,0 +1,21 @@
+package com.example.pss.domain.user.facade;
+
+import com.example.pss.domain.user.domain.User;
+import com.example.pss.domain.user.domain.repository.UserRepository;
+import com.example.pss.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String nickname = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userRepository.findByNickname(nickname)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
