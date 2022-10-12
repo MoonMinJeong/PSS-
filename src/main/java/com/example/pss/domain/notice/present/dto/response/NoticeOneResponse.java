e37bfc1aa51feeb5d967d7bd72adcfebf508c002
munjeongmin <3330kys@dsm.hs.kr>
2022-10-12 17:20:52 +0900

    EDIT :: Response QueryProjection

--- src/main/java/com/example/pss/domain/notice/present/dto/response/NoticeOneResponse.java
+++ src/main/java/com/example/pss/domain/notice/present/dto/response/NoticeOneResponse.java
@@ -1,4 +1,13 @@
 package com.example.pss.domain.notice.present.dto.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
 public class NoticeOneResponse {
+    private String title;
+    private String content;
 }
--- src/main/java/com/example/pss/domain/notice/present/dto/response/NoticeResponse.java
+++ src/main/java/com/example/pss/domain/notice/present/dto/response/NoticeResponse.java
@@ -1,6 +1,7 @@
 package com.example.pss.domain.notice.present.dto.response;
 
 import com.example.pss.domain.stack.domain.Stack;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,20 +13,41 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 public class NoticeResponse {
-    private String title;
-    private String imageUrl;
-    private String introduction;
+    private List<NoticeDto> noticeList;
 
-    private int viewCount;
-    private float stars;
-    private int likes;
+    @Getter
+    @Builder
+    public static class NoticeDto {
+        private String title;
+        private String imageUrl;
+        private String introduction;
+        private List<Stack> stacks;
 
-    private boolean isMine;
-    private String nickname;
-    private String profileImage;
-    private String email;
+        private int viewCount;
+        private float stars;
+        private int likes;
 
-    private List<Stack> stacks;
+        private boolean isMine;
+        private boolean isLike;
+        private String nickname;
+        private String profileImage;
 
-    private LocalDateTime createTime;
+        private LocalDateTime createTime;
+
+        @QueryProjection
+        public NoticeDto(
+                String title, String imageUrl, String introduction, int viewCount,
+                float stars, int likes, boolean isMine, boolean isLike, String nickname,
+                String profileImage, String email, List<Stack> stacks, LocalDateTime createTime) {
+            this.title = title;
+            this.imageUrl = imageUrl;
+            this.introduction = introduction;
+            this.stacks = stacks;
+            this.nickname = nickname;
+            this.profileImage = profileImage;
+            this.likes = likes;
+            this.stars = stars;
+            this.viewCount = viewCount;
+        }
+    }
 }
