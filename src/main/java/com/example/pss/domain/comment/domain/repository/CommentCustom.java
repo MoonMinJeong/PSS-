e95ff16cc2f2eb18b9fbb80225fdf46ef519922e
munjeongmin <3330kys@dsm.hs.kr>
2022-10-18 22:44:54 +0900

    ADD :: comment list with replies

--- src/main/java/com/example/pss/domain/comment/domain/repository/CommentCustom.java
+++ src/main/java/com/example/pss/domain/comment/domain/repository/CommentCustom.java
@@ -1,9 +1,12 @@
 package com.example.pss.domain.comment.domain.repository;
 
+import com.example.pss.domain.comment.domain.Comment;
 import com.example.pss.domain.reply.domain.Reply;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CommentCustom {
+    List<Comment> getByNotice(UUID noticeId);
     List<Reply> getByComment(Long commentId);
 }
--- src/main/java/com/example/pss/domain/reply/service/ReplyService.java
+++ src/main/java/com/example/pss/domain/reply/service/ReplyService.java
@@ -26,6 +26,7 @@ public class ReplyService {
                         .content(request.getContent())
                         .user(user)
                         .comment(comment)
+                        .isMine(true)
                         .build()
         );
     }
